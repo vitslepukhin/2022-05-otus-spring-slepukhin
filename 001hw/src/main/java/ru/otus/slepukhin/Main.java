973b9b3db48feafd628e9e2de2c8260bb7ade738
Vitalii Slepukhin <vitslepukhin@gmail.com>
2022-05-22 13:45:44 +0300

    Add 001hw project

---
+++ 001hw/src/main/java/ru/otus/slepukhin/Main.java
@@ -0,0 +1,10 @@
+package ru.otus.slepukhin;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+    }
+}
