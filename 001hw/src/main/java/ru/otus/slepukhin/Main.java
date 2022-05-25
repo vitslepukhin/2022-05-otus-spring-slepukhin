105104bf3850d495b618d1ff89796e7b8a6581b8
Vitalii Slepukhin <vitslepukhin@gmail.com>
2022-05-25 09:46:10 +0300

    Config context and Main

--- 001hw/src/main/java/ru/otus/slepukhin/Main.java
+++ 001hw/src/main/java/ru/otus/slepukhin/Main.java
@@ -1,10 +1,14 @@
 package ru.otus.slepukhin;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.slepukhin.service.QuizEngine;
 
 public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        QuizEngine quizEngine = context.getBean(QuizEngine.class);
+        quizEngine.process();
+        quizEngine.showResult();
     }
 }
